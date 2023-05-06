b945ed67fe346381e27d2a560f1acb007952f107
Daria Burtseva <qmemorais@gmail.com>
2023-05-06 17:26:38 +0200

    refactore userService

--- ExerciseApp/UserService.cs
+++ ExerciseApp/UserService.cs
@@ -10,78 +10,58 @@ namespace ExerciseApp
             _customerRepository = new CustomerRepository();
         }
 
-        public bool AddUser(string firеnam, string surname, string email, DateTime dateOfBirth, int customerId)
+        public bool AddUser(string firstname, string surname, string email, DateTime dateOfBirth, int customerId)
         {
-            if (string.IsNullOrEmpty(firеnam) || string.IsNullOrEmpty(surname))
-            {
-                return false;
-            }
-
-            if (email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
-            if (CalculateAge(dateOfBirth) < 21)
-            {
-                return false;
-            }
-
-            var user = new User
-            {
-                Customer = _customerRepository.GetById(customerId),
-                DateOfBirth = dateOfBirth,
-                EmailAddress = email,
-                Firstname = firеnam,
-                Surname = surname
-            };
+            var user = CreateUser(firstname, surname, email, dateOfBirth, customerId);
 
             SetCreditLimit(user);
 
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
-            
+            if (!Validate(user)) return false;
+
             UserDataAccess.AddUser(user);
 
             return true;
         }
 
+        private User CreateUser(string firstname, string surname, string email, DateTime dateOfBirth, int customerId)
+            => new User { Firstname = firstname, Surname = surname, EmailAddress = email, DateOfBirth = dateOfBirth, Customer = _customerRepository.GetById(customerId) };
+
         private void SetCreditLimit(User user)
         {
-            if (user.Customer.Name == "VeryImportantCustomer")
-            {
-                // Перевірити кредитний ліміт
-                user.HasCreditLimit = false;
-            }
-            else
+            if (user.Customer.Name != "VeryImportantCustomer")
             {
                 // Do credit check
                 user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
 
-                    if (user.Customer.Name == "ImportantCustomer")
-                        creditLimit = creditLimit * 2; //подвоїти кредитний ліміт
+                using var userCreditService = new UserCreditServiceClient();
 
-                    user.CreditLimit = creditLimit;
-                }
+                var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
+
+                if (user.Customer.Name == "ImportantCustomer")
+                    creditLimit *= 2; //подвоїти кредитний ліміт
+
+                user.CreditLimit = creditLimit;
             }
         }
 
         private int CalculateAge(DateTime dateOfBirth)
         {
             var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
+            var age = now.Year - dateOfBirth.Year;
 
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
-            {
-                age--;
-            }
+            return now.DayOfYear < dateOfBirth.DayOfYear
+                ? age-- : age;
+        }
 
-            return age;
+        private bool Validate(User user)
+        {
+            if (string.IsNullOrEmpty(user.Firstname) || string.IsNullOrEmpty(user.Surname)
+                || (user.EmailAddress.Contains("@") && !user.EmailAddress.Contains("."))
+                || CalculateAge(user.DateOfBirth) < 21
+                || user.HasCreditLimit && user.CreditLimit < 500)
+                return false;
+
+            return true;
         }
     }
 }
